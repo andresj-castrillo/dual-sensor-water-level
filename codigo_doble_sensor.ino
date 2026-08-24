753685d6f44bded9c954f7b7db9595f97831268f
andresj-castrillo <ancastrillo@javeriana.edu.co>
2026-08-23 23:42:35 -0500

    feat: add mandatory serial calibration flow for load cell

--- codigo_doble_sensor.ino
+++ codigo_doble_sensor.ino
@@ -90,12 +90,72 @@ float getDistanceCalibrated() {
   return (d_fil * cal_factor) + cal_offset;
 }
 
+// Calibracion secuemcial obligatorio en consola
+void cambiarDimensionesGalga() {
+  Serial.println("\n--- CONFIGURACION DE DIMENSIONES DEL RECIPIENTE (GALGA) ---");
+  Serial.println("Ingrese el radio interno en cm:");
+  while (!Serial.available());
+  float r_temp = Serial.parseFloat();
+  if(r_temp > 0) radio_cm = r_temp;
+  Serial.print("Radio fijado: "); Serial.print(radio_cm); Serial.println(" cm");
+
+  while (Serial.available()) Serial.read(); 
+
+  Serial.println("Ingrese la altura interna en cm:");
+  while (!Serial.available());
+  float h_temp = Serial.parseFloat();
+  if(h_temp > 0) altura_cm = h_temp;
+  Serial.print("Altura fijada: "); Serial.print(altura_cm); Serial.println(" cm");
+
+  EEPROM.put(radio_eepromAdress, radio_cm);
+  EEPROM.put(altura_eepromAdress, altura_cm);
+  EEPROM.commit();
+}
+
+void calibrarGalga() {
+  Serial.println("\n--- CALIBRACION OBLIGATORIA DE LA GALGA ---");
+  Serial.println("1. Retire todo el peso de la balanza.");
+  Serial.println("Envie 't' por consola para establecer el TARE:");
+  
+  boolean _resume = false;
+  while (!_resume) {
+    LoadCell.update();
+    if (Serial.available() > 0) {
+      char inByte = Serial.read();
+      if (inByte == 't') LoadCell.tareNoDelay();
+    }
+    if (LoadCell.getTareStatus()) _resume = true;
+  }
+
+  while (Serial.available()) Serial.read();
+
+  Serial.println("\n2. Coloque un peso conocido sobre la galga.");
+  Serial.println("Ingrese el valor exacto de ese peso en gramos:");
+  float known_mass = 0;
+  while (known_mass == 0) {
+    LoadCell.update();
+    if (Serial.available() > 0) known_mass = Serial.parseFloat();
+  }
+  
+  LoadCell.refreshDataSet();
+  float newCalibrationValue = LoadCell.getNewCalibration(known_mass);
+  
+  if(isnan(newCalibrationValue) || newCalibrationValue == 0) newCalibrationValue = 1.0;
+  
+  EEPROM.put(calVal_eepromAdress, newCalibrationValue);
+  EEPROM.commit();
+  
+  LoadCell.setCalFactor(newCalibrationValue);
+  Serial.print("Factor de calibración guardado: "); Serial.println(newCalibrationValue);
+}
+
+
 // Setup
 void setup() {
   Serial.begin(57600);
   delay(10);
 
-  // Configuracion de pines
+  // Configuracion de pinesf
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, OUTPUT);
 
@@ -111,8 +171,12 @@ void setup() {
   unsigned long stabilizingtime = 2000;
   LoadCell.start(stabilizingtime, false);
 
-
   
+  // Ejecución de calibraciones secuenciales
+  cambiarDimensionesGalga();
+  calibrarGalga();
+
+
   // Conexion WIFI
   WiFi.begin(ssid, password);
   Serial.print("Estableciendo conexion Wi-Fi");
